834f0cbc95dd615d54b02d4d2e4b45bc75e3933f
Jash Patel <jashpp@gmail.com>
2013-08-21 02:24:33 -0400

    Split time into start_time/end_time for employer info session dataset

--- EmployerInfoSessions/sessions.py
+++ EmployerInfoSessions/sessions.py
@@ -26,6 +26,9 @@ def parse_link(html):
 		link = ""
 	return link
 
+def parse_time(html):
+	return html.split(" - ")
+
 # parsing Fall 2013
 for month in ["2013Sep", "2013Oct"]:
 	html = urllib2.urlopen(CECA_URL%month).read()
@@ -34,7 +37,7 @@ for month in ["2013Sep", "2013Oct"]:
 	ids = get_ids(html)
 	employers = get_by_label("Employer:", html)
 	dates = get_by_label("Date:", html)
-	times = get_by_label("Time:", html)
+	times = map(parse_time, get_by_label("Time:", html))
 	locations = get_by_label("Location:", html)
 	websites = map(parse_link, get_by_label("Web Site:", html))
 	others = get_others(html)
@@ -49,7 +52,8 @@ for month in ["2013Sep", "2013Oct"]:
 		session["id"] = ids[i]
 		session["employer"] = employers[i]
 		session["date"] = dates[i]
-		session["time"] = times[i]
+		session["start_time"] = times[i][0]
+		session["end_time"] = times[i][1]
 		session["location"] = locations[i]
 		session["website"] = websites[i]
 		session["coop/grad"] = others[i][0]
@@ -63,6 +67,6 @@ for month in ["2013Sep", "2013Oct"]:
 
 with open('sessions.csv', 'wb') as f:
 	writer = csv.writer(f)
-	writer.writerow(["id", "employer", "date", "time", "location", "website", "coop/grad", "programs", "info"])
+	writer.writerow(["id", "employer", "date", "start_time", "end_time", "location", "website", "coop/grad", "programs", "info"])
 	for session in sessions:
-		writer.writerow([session["id"], session["employer"], session["date"], session["time"], session["location"], session["website"], session["coop/grad"], session["programs"], session["info"]])
+		writer.writerow([session["id"], session["employer"], session["date"], session["start_time"], session["end_time"], session["location"], session["website"], session["coop/grad"], session["programs"], session["info"]])
